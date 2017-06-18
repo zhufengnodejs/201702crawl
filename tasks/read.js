14827570bd1a1378918bca8228d566c487a59110
zhufengnodejs <zhufengnodejs@126.com>
2017-06-18 15:23:26 +0800

    1.读取电影列表

--- tasks/read.js
+++ tasks/read.js
@@ -11,7 +11,16 @@ module.exports = function(url,callback){
   request({url,encoding:null},function(err,response,body){
        body = iconv.decode(body,'gbk');
        let $ = cheerio.load(body);
-
+       let movies = [];
+       $('.keyword a.list-title').each(function(){
+           let $this = $(this);
+           let movie = {
+               name:$this.text(),
+               url:$this.attr('href')
+           }
+           movies.push(movie);
+       });
+       callback(err,movies);
   });
 }
 
