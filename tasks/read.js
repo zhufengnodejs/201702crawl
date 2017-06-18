d293e54dfc3c2b6f35d856805e8863563dcd366f
zhufengnodejs <zhufengnodejs@126.com>
2017-06-18 15:13:40 +0800

    init

---
+++ tasks/read.js
@@ -0,0 +1,20 @@
+/**
+ * 1. 向网址发出请求得到响应体
+ * 2. 把得到的Buffer转成字符串
+ * 3. 从字符串提取我们需要的内容
+ * 4. 把提取到的结果传入回调函数
+ */
+let request = require('request');
+let iconv = require('iconv-lite');
+let cheerio = require('cheerio');
+module.exports = function(url,callback){
+  request({url,encoding:null},function(err,response,body){
+       body = iconv.decode(body,'gbk');
+       let $ = cheerio.load(body);
+
+  });
+}
+
+module.exports('http://top.baidu.com/buzz?b=26&c=1',function(err,movies){
+    console.log(movies);
+});
