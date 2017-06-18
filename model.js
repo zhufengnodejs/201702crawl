ecf85bcc3fe70828ac8210fb15dfcc4d8552d496
zhufengnodejs <zhufengnodejs@126.com>
2017-06-18 15:39:17 +0800

    2.把电影数组保存到mongodb数据库中

---
+++ model.js
@@ -0,0 +1,8 @@
+let mongoose = require('mongoose');
+//连接数据库
+mongoose.connect('mongodb://127.0.0.1/201702crawl');
+let MovieSchema = new mongoose.Schema({
+    name:String,
+    url:String
+})
+module.exports = mongoose.model('Movie',MovieSchema);
---
+++ tasks/write.js
@@ -0,0 +1,14 @@
+let Movie = require('../model');
+let async = require('async');
+//把电影数组保存到mongodb数据库中
+module.exports = function(movies,callback){
+  async.forEach(movies,function(movie,cb){
+    Movie.create(movie,cb);
+  },callback);
+  //Movie.create(movies,callback);
+}
+
+module.exports([
+    { name: '嫌疑人x的献身',
+        url: 'http://www.baidu.com/baidu?cl=3&tn=SE_baiduhomet8_jmjb7mjw&fr=top1000&wd=%CF%D3%D2%C9%C8%CBx%B5%C4%CF%D7%C9%ED' }
+],function(){});
